4ae286f37e525955ae7de80b7201b21051b30c70
Andrey Chechkin <andrey.chechkin@me.com>
2016-07-14 22:26:45 +0300

    Complete calculator

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,8 @@
 'use strict'
 
 //Second Home Work
-let secondHW = require("./modules/second-home-work/main-home-work.js");
-let secondHWCalculator = require("./modules/second-home-work/calc-module.js");
+const secondHW = require("./modules/second-home-work/main-home-work.js");
+const secondHWCalculator = require("./modules/second-home-work/calc-module.js");
 
 var allNumbers = [1, 2, 4, 5, 6, 7, 8],
 someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
@@ -19,21 +19,26 @@ let isNumber = function (val) {
 	return typeof val === 'number';
 }
 
-// //#1
-// console.log("#1");
-// console.log(secondHW.isAllTrue(allNumbers, isNumber));
-// console.log(secondHW.isAllTrue(someNumbers, isNumber));
-// console.log(secondHW.isAllTrue(noNumbers, isNumber));
-// //console.log(secondHW.isAllTrue([], isNumber));
-//
-// //#2
-// console.log("#2");
-// console.log(secondHW.isSomeTrue(someNumbers, isNumber));
-// console.log(secondHW.isSomeTrue(someNumbers, isNumber));
-// console.log(secondHW.isSomeTrue(noNumbers, isNumber));
-// //console.log(secondHW.isAllTrue([], isNumber));
+//#1
+console.log("################1################");
+console.log(secondHW.isAllTrue(allNumbers, isNumber));
+console.log(secondHW.isAllTrue(someNumbers, isNumber));
+console.log(secondHW.isAllTrue(noNumbers, isNumber));
+//console.log(secondHW.isAllTrue([], isNumber));
 
+//#2
+console.log("################2################");
+console.log(secondHW.isSomeTrue(someNumbers, isNumber));
+console.log(secondHW.isSomeTrue(someNumbers, isNumber));
+console.log(secondHW.isSomeTrue(noNumbers, isNumber));
+//console.log(secondHW.isAllTrue([], isNumber));
+
+//#3
 let myCalculator = secondHWCalculator.calculator(100);
 
+console.log("################calculator################");
 console.log(myCalculator.sum(1, 2, 3));
 console.log(myCalculator.dif(10, 20));
+console.log(myCalculator.div(2, 2));
+console.log(myCalculator.mul(2, 2));
+//console.log(myCalculator.div(0, 2));
--- scripts/modules/second-home-work/calc-module.js
+++ scripts/modules/second-home-work/calc-module.js
@@ -10,16 +10,34 @@
 let calculator = function(firstNumber){
 	return {
 		sum: function(){
+			let res = firstNumber;
 			for(let i in arguments){
-				firstNumber += arguments[i];
+				res += arguments[i];
 			}
-			return firstNumber;
+			return res;
 		},
 		dif: function(){
+			let res = firstNumber;
 			for(let i in arguments){
-				firstNumber -= arguments[i];
+				res -= arguments[i];
 			}
-			return firstNumber;
+			return res;
+		},
+		div: function(){
+			let res = firstNumber;
+			for(let i in arguments){
+				if (arguments[i] == 0)
+					throw new Error("На 0 делить нельзя!");
+				res /= arguments[i];
+			}
+			return res;
+		},
+		mul: function(){
+			let res = firstNumber;
+			for(let i in arguments){
+				res *= arguments[i];
+			}
+			return res;
 		}
 	}
 }
