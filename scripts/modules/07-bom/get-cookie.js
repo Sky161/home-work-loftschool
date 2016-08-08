48b4f2b7859b5fbbfe98d6db349d377d497d218f
Andrey Chechkin <andrey.chechkin@me.com>
2016-08-08 10:49:02 +0300

    Build object cookie

--- scripts/modules/07-bom/get-cookie.js
+++ scripts/modules/07-bom/get-cookie.js
@@ -9,13 +9,24 @@
 
 const getCookie = () => {
 	let date = new Date;
-	let cookies = document.cookie;
+	let cookieObj = {};
+	let cookieArr = [];
 
 	date.setDate(date.getDate() + 1);
 
-	cookies = `name=Andrey; surname=Chechkin; age=23; learn=Javascript path=/; expires=${date.toUTCString()}`; 
+	document.cookie = `name=Andrey; path=/; expires=${date.toUTCString()}`;
+	document.cookie = `surname=Chechkin; path=/; expires=${date.toUTCString()}`;
+	document.cookie = `age=23; path=/; expires=${date.toUTCString()}`;
+	document.cookie = `learn=Javascript; path=/; expires=${date.toUTCString()}`;
 
-	console.log(cookies);
+	cookieArr = document.cookie.split(";");
+
+	cookieArr.forEach((item) => {
+		item = item.split("=");
+		cookieObj[item[0]] = item[1];
+	});
+
+	console.log(cookieObj);
 }
 
 module.exports = { getCookie }
