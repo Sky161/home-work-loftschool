d6bed9a84cc80e7e620db140a5fea65e560ec603
Andrey Chechkin <andrey.chechkin@me.com>
2016-09-01 22:23:28 +0300

    Add http server, response request list files

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,10 @@
 'use strict';
 
 const fs = require("fs");
+const http = require("http");
+const path = require("path");
 let separator = "";
+let content = "";
 
 const readDir = (dirSearch) => {
   let dirs = fs.readdirSync(dirSearch);
@@ -19,12 +22,30 @@ const readDir = (dirSearch) => {
     let stat = fs.statSync(dir);
     let size = stat.size / 1024;
 
-    console.log(`${separator} ${dir} (${size}kb)`);
-
     if(stat.isDirectory()) {
       readDir(dir);
     }
+
+    content += `${separator} ${dir} (${size}kb) \n`;
   }
 }
 
-readDir("./scripts");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  fs.access("." + req.url, fs.F_OK, function(err) {
+    if (!err) {
+      readDir("." + req.url);
+      res.end(content);
+      process.nextTick(() => {
+        content = "";
+        separator = "";
+      });
+    } else {
+      res.end("Путь не найден!");
+    }
+  });
+});
+
+server.listen("7777", "localhost", () => {
+  console.log("Сервер запущен по адресу http://localhost:7777");
+});
